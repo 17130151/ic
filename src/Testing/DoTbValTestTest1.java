75a07f67a0a73bdcc851fd93a66363e1e98bdbc0
Bobinaa <m9631340530mqweR>
2018-11-26 12:15:52 +0300

    new tests

---
+++ src/Testing/DoTbValTestTest1.java
@@ -0,0 +1,26 @@
+package Testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import Program.FrameUI;
+
+public class DoTbValTestTest1 {
+
+	@Test
+	public void testKtVal() {
+		FrameUI frame = new FrameUI("��������� �����������", 820, 780); /*������� ���������� ������ FrameUI*/
+		assertEquals(Double.toString(frame.TbVal(0)),"1215.0"); /*�������� ������������ ���������� � ��������� ����������*/
+		assertEquals(Double.toString(frame.TbVal(1)),"2375.0"); /*�������� ������������ ���������� � ��������� ����������*/
+		assertEquals(Double.toString(frame.TbVal(2)),"2965.0"); /*�������� ������������ ���������� � ��������� ����������*/
+		assertEquals(Double.toString(frame.TbVal(3)),"2025.0"); /*�������� ������������ ���������� � ��������� ����������*/
+		assertEquals(Double.toString(frame.TbVal(4)),"3240.0"); /*�������� ������������ ���������� � ��������� ����������*/
+		assertEquals(Double.toString(frame.TbVal(5)),"1620.0"); /*�������� ������������ ���������� � ��������� ����������*/
+		assertEquals(Double.toString(frame.TbVal(6)),"2025.0"); /*�������� ������������ ���������� � ��������� ����������*/
+		assertEquals(Double.toString(frame.TbVal(7)),"1620.0"); /*�������� ������������ ���������� � ��������� ����������*/
+		assertEquals(Double.toString(frame.TbVal(8)),"1010.0"); /*�������� ������������ ���������� � ��������� ����������*/
+		assertEquals(Double.toString(frame.TbVal(9)),"395.0"); /*�������� ������������ ���������� � ��������� ����������*/
+	}
+
+}
