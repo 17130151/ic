a22b14160c1d420290123b597165acb83b781d40
17130151 <44563753+17130151@users.noreply.github.com>
2018-12-01 09:49:13 +0300

    Update DoTbValTestTest1.java

--- src/Testing/DoTbValTestTest1.java
+++ src/Testing/DoTbValTestTest1.java
@@ -1,3 +1,4 @@
+
 package Testing;
 
 import static org.junit.Assert.*;
@@ -6,21 +7,21 @@ import org.junit.Test;
 
 import Program.FrameUI;
 
-public class DoTbValTestTest1 {
+public class DoTbValTest {
 
-	@Test
-	public void testKtVal() {
-		FrameUI frame = new FrameUI("��������� �����������", 820, 780); /*������� ���������� ������ FrameUI*/
-		assertEquals(Double.toString(frame.TbVal(0)),"1215.0"); /*�������� ������������ ���������� � ��������� ����������*/
-		assertEquals(Double.toString(frame.TbVal(1)),"2375.0"); /*�������� ������������ ���������� � ��������� ����������*/
-		assertEquals(Double.toString(frame.TbVal(2)),"2965.0"); /*�������� ������������ ���������� � ��������� ����������*/
-		assertEquals(Double.toString(frame.TbVal(3)),"2025.0"); /*�������� ������������ ���������� � ��������� ����������*/
-		assertEquals(Double.toString(frame.TbVal(4)),"3240.0"); /*�������� ������������ ���������� � ��������� ����������*/
-		assertEquals(Double.toString(frame.TbVal(5)),"1620.0"); /*�������� ������������ ���������� � ��������� ����������*/
-		assertEquals(Double.toString(frame.TbVal(6)),"2025.0"); /*�������� ������������ ���������� � ��������� ����������*/
-		assertEquals(Double.toString(frame.TbVal(7)),"1620.0"); /*�������� ������������ ���������� � ��������� ����������*/
-		assertEquals(Double.toString(frame.TbVal(8)),"1010.0"); /*�������� ������������ ���������� � ��������� ����������*/
-		assertEquals(Double.toString(frame.TbVal(9)),"395.0"); /*�������� ������������ ���������� � ��������� ����������*/
-	}
+@Test
+public void testKtVal() {
+FrameUI frame = new FrameUI("Страховой калькулятор", 820, 780); /*Создани экземпляра класса FrameUI*/
+assertEquals(Double.toString(frame.TbVal(0)),"1215.0"); /*Проверка соответствия ожидаемого и реального результата*/
+assertEquals(Double.toString(frame.TbVal(1)),"2375.0"); /*Проверка соответствия ожидаемого и реального результата*/
+assertEquals(Double.toString(frame.TbVal(2)),"2965.0"); /*Проверка соответствия ожидаемого и реального результата*/
+assertEquals(Double.toString(frame.TbVal(3)),"2025.0"); /*Проверка соответствия ожидаемого и реального 1620.0результата*/
+assertEquals(Double.toString(frame.TbVal(4)),"3240.0"); /*Проверка соответствия ожидаемого и реального результата*/
+assertEquals(Double.toString(frame.TbVal(5)),"1620.0"); /*Проверка соответствия ожидаемого и реального результата*/
+assertEquals(Double.toString(frame.TbVal(6)),"2025.0"); /*Проверка соответствия ожидаемого и реального результата*/
+assertEquals(Double.toString(frame.TbVal(7)),"1620.0"); /*Проверка соответствия ожидаемого и реального результата*/
+assertEquals(Double.toString(frame.TbVal(8)),"1010.0"); /*Проверка соответствия ожидаемого и реального результата*/
+assertEquals(Double.toString(frame.TbVal(9)),"395.0"); /*Проверка соответствия ожидаемого и реального результата*/
+}
 
 }
