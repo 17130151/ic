581bed589aadf9a6cf13b40cd08ed7f7c838d76c
17130151 <44563753+17130151@users.noreply.github.com>
2018-12-01 09:48:11 +0300

    Update DoCalculationTestTwo1.java

--- src/Testing/DoCalculationTestTwo1.java
+++ src/Testing/DoCalculationTestTwo1.java
@@ -1,3 +1,4 @@
+
 package Testing;
 
 import static org.junit.Assert.*;
@@ -6,12 +7,12 @@ import org.junit.Test;
 
 import Program.FrameUI;
 
-public class DoCalculationTestTwo1 {
+public class DoCalculationTestTwo {
 
-	@Test
-	public void testCalculation() {
-		FrameUI frame = new FrameUI("��������� �����������", 820, 780); /*������� ���������� ������ FrameUI*/
-		assertEquals("42483.0" ,frame.Calculation(2965.0, 2.0, 2.45, 1.0 ,1.8, 1.2, 0.95,1.5,0.95));  /*�������� ������������ ���������� � ��������� ����������*/
-	}
+@Test
+public void testCalculation() {
+FrameUI frame = new FrameUI("Страховой калькулятор", 820, 780); 
+assertEquals("42483.0" ,frame.Calculation(2965.0, 2.0, 2.45, 1.0 ,1.8, 1.2, 0.95,1.5,0.95)); 
+}
 
 }
