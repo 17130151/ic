f8068159fe6bc29bfe734b7477593c6233435f10
17130151 <44563753+17130151@users.noreply.github.com>
2018-12-01 09:45:54 +0300

    Update Starter.java

--- src/Program/Starter.java
+++ src/Program/Starter.java
@@ -2,7 +2,6 @@ package Program;
 
 public class Starter {
 	   public static void main(String[] args) {
-	        System.out.println("> Запуск работы страхового калькулятора"); /*Вывод строки в консоль*/
-	        FrameUI frame = new FrameUI ("Страховой калькулятор", 820, 735); /*Создание экземпляра класса Frame*/
+	        FrameUI frame = new FrameUI ("РЎС‚СЂР°С…РѕРІРѕР№ РєР°Р»СЊРєСѓР»СЏС‚РѕСЂ", 820, 735);
 		    }
 }
